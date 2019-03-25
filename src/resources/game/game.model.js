c4f9426978e66c28b600a070b87496cfd28555df
josepplloo <josepplloo@gmail.com>
2019-03-25 02:57:12 -0500

    tidying up the project

--- src/resources/game/game.model.js
+++ src/resources/game/game.model.js
@@ -2,15 +2,14 @@ import mongoose from 'mongoose'
 
 const gameSchema = new mongoose.Schema(
   {
+    due: Date,
     name: {
       type: String,
       required: true,
       trim: true,
       maxlength: 50
-    },
-    due: Date
+    }
   },
   { timestamps: true }
 )
-gameSchema.index({ list: 1, name: 1 }, { unique: true })
 export const Game = mongoose.model('game', gameSchema)
